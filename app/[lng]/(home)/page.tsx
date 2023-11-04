4b43fa98af1fc657fb9f127d4ea3014359ef65c3
HamzaSourani <hamzasourani98@gmail.com>
2023-11-04 10:02:16 -0700

    make links in navbar capitalize

--- app/[lng]/(home)/page.tsx
+++ app/[lng]/(home)/page.tsx
@@ -25,7 +25,6 @@ export default async function Home({ params: { lng } }: LngParams) {
           <ContactSidebar />
         </div>
       </div>
-      <div className="h-[1500px]"></div>
       <Particle />
     </>
   );
